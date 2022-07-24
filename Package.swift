c1958ca44fc00373454ea17dff95449cbac9aa34
Wolf Dieter Dallinger <wolf.dieter@dallinger.de>
2022-07-25 01:19:28 +0200

    0.0.12: Updated package.swift and documentation.

--- Package.swift
+++ Package.swift
@@ -14,6 +14,10 @@ let package = Package(
             name: "LockApp",
             targets: ["LockApp"]),
     ],
+    /* To generate static documentation in a deleted docs directory enter in twrminal:
+     cd ~/Documents/Apps/RemoteSwiftPackages/LockApp
+     swift package --allow-writing-to-directory ~/Documents/Apps/RemoteSwiftPackages/LockApp/docs generate-documentation --target LockApp --disable-indexing --transform-for-static-hosting --hosting-base-path LockApp --output-path ~/Documents/Apps/RemoteSwiftPackages/LockApp/docs
+     */
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
